7b653c775f6023b5247a15ef789283b167607de0
Yasir Ahmad <yasir.ahmad0810@gmail.com>
2023-02-21 14:39:16 +0530

    add

---
+++ Module2/Addition.java
@@ -0,0 +1,11 @@
+public class Addition{
+	public static void main(String[] args){
+		int a=27;
+		int b=45;
+		int c=add(a,b);
+		System.out.println("Sum = "+c);
+	}
+	static int add(int a,int b){
+		return a+b;
+	}
+}
